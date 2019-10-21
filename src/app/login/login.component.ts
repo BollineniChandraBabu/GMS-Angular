a2ef354d51fab10b250e9f3d3e9c04f769fa5dd5
CHANDRA BABU BOLLINENI <chandrababubollineni416@gmail.com>
2019-10-21 19:16:11 +0530

    updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,3 +19,27 @@ export class LoginComponent implements OnInit {
     const modalRef = this.modalService.open("content");
   
 }}
+
+export class NgbdModalLogin {
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content:any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+}
