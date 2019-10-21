2f74a21da01bc94f3bee2bca841c78a79a51cc9e
Chandra Babu Bollineni <chandrababubollineni416@gmail.com>
2019-10-21 10:02:24 +0530

    routing added

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private modalService: NgbModal) { }
+
+  ngOnInit() {
+    this.msg();
+  }
+ 
+ msg() {
+    const modalRef = this.modalService.open("content");
+  
+}}
