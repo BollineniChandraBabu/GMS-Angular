a8e9e4926f48f270bc9be1480d915f7fb1a7841d
CHANDRA BABU BOLLINENI <chandrababubollineni416@gmail.com>
2019-10-23 19:54:44 +0530

    23/10/2019

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, APP_BOOTSTRAP_LISTENER } from '@angular/core';
+import {NgbdModalBasic} from './modal-basic'
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
+
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-
+import {NgbdModalLogin} from './modal-login';
 @NgModule({
   declarations: [
     AppComponent,
